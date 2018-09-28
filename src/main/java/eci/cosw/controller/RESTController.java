ead5f181a0cae9f55871eb6d987387dea8ae3f08
Fabian Ardila <fabianra97@gmail.com>
2018-09-27 23:00:53 -0500

    File Uploader API implemented

--- src/main/java/eci/cosw/controller/RESTController.java
+++ src/main/java/eci/cosw/controller/RESTController.java
@@ -1,6 +1,8 @@
 package eci.cosw.controller;
 
 
+import com.mongodb.client.gridfs.model.GridFSFile;
+import eci.cosw.data.TodoRepository;
 import eci.cosw.data.model.Todo;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
@@ -10,19 +12,36 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 @RequestMapping("api")
 @RestController
 public class RESTController {
-
-
-   //TODO inject components (TodoRepository and GridFsTemplate)
+    
+    @Autowired
+    GridFsTemplate gridFsTemplate;
+    
+    @Autowired
+    TodoRepository todoRepository;
 
     @RequestMapping("/files/{filename}")
     public ResponseEntity<InputStreamResource> getFileByName(@PathVariable String filename) throws IOException {
 
-        //TODO implement method
-        return null;
+        GridFSFile file = gridFsTemplate.findOne(new Query().addCriteria(Criteria.where("filename").is(filename)));
+        if (file == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            GridFsResource resource = gridFsTemplate.getResource(file.getFilename());
+            return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(resource.getContentType()))
+                .body(new InputStreamResource(resource.getInputStream()));
+        }
 
     }
 
